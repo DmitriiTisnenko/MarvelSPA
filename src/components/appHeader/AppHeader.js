db321a1a1f1eacb1c2bdb0e216dc016ff51af394
DmitriiTisnenko <TisnenkoDI@gmail.com>
2024-02-10 13:39:29 +0200

    build is ready

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -4,15 +4,15 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="https://www.codewars.com/kata/56dec885c54a926dcd001095/solutions/javascript?filter=all&sort=clever&invalids=false">
+                <a target='_blank' rel="noreferrer" href="https://www.marvel.com">
                     <span>Marvel</span> information portal
                 </a>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="https://developer.mozilla.org/ru/docs/Web/JavaScript/Language_overview">Characters</a></li>
+                    <li><a target='_blank' rel="noreferrer" href="https://www.marvel.com/characters">Characters</a></li>
                     /
-                    <li><a href="https://ru.react.js.org/docs/introducing-jsx.html">Comics</a></li>
+                    <li><a target='_blank' rel="noreferrer" href="http://marvel.com/comics">Comics</a></li>
                 </ul>
             </nav>
         </header>
